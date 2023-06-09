83dffba796eeee275958ee923eba15d903aed11b
Raul Mendez Poblete <82802107+Siname22@users.noreply.github.com>
2023-06-09 16:50:54 +0200

    .

--- project/config.py
+++ project/config.py
@@ -4,11 +4,11 @@ class BaseConfig:
     """Base Confilguration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysql://root:@localhost/books_gallery'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/books_gallery'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     CORS_HEADERS = 'Content-Type'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
